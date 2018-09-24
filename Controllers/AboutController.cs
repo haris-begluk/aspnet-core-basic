7b6edbb1ac1506e3b6f7999c0fadb2c12f954cb8
haris-begluk <harisbegluk@gmail.com>
2018-09-24 10:32:23 +0200

    Attribute routing in MVC

--- Controllers/AboutController.cs
+++ Controllers/AboutController.cs
@@ -1,12 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+
 namespace aspnet_core_basic.Controllers
 {
+    [Route("about")]
     public class AboutController
     {
+        [Route("")]
         public string Phone()
         {
             return "44353636";
         }
-
+        [Route("address")]
         public string Address()
         {
             return "USA-New York";
