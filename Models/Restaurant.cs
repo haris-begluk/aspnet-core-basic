b292b6e429399de91d44df6bd49acd8138826812
haris-begluk <harisbegluk@gmail.com>
2018-09-26 11:13:36 +0200

    Model validation with data annotations in MVC

--- Models/Restaurant.cs
+++ Models/Restaurant.cs
@@ -1,8 +1,12 @@
+using System.ComponentModel.DataAnnotations;
 namespace aspnet_core_basic.Models
 {
     public class Restaurant
     {
         public int Id { get; set; }
+        [Display(Name = "Restaurant Name")]
+        //[DataType(DataType.Password)] 
+        [Required, MaxLength(80)]
         public string Name { get; set; }
         public CuisineType Cuisine { get; set; }
     }
--- ViewModels/RestaurantEditModel.cs
+++ ViewModels/RestaurantEditModel.cs
@@ -1,9 +1,11 @@
+using System.ComponentModel.DataAnnotations;
 using aspnet_core_basic.Models;
 
 namespace aspnet_core_basic.ViewModels
 {
     public class RestaurantEditModel
     {
+        [Required, MaxLength(80)]
         public string Name { get; set; }
         public CuisineType Cuisine { get; set; }
     }
