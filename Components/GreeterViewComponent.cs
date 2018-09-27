900402ce22675fe01e5348c8ac2eacee046a24ba
haris-begluk <harisbegluk@gmail.com>
2018-09-27 15:07:35 +0200

    View components

---
+++ Components/GreeterViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using aspnet_core_basic.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace aspnet_core_basic.Components
+{
+    public class GreeterViewComponent :ViewComponent
+    {
+        private IGreeter _greeter;
+
+        public GreeterViewComponent(IGreeter greeter)
+        {
+            _greeter = greeter;
+        } 
+        public IViewComponentResult Invoke()
+        {
+            var model = _greeter.GetMessageOfTheDay();
+            return View("Default", model);
+        }
+    }
+}
